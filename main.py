4f75db546ed93a39a1882e34cf207b64be05bee9
flohms18 <trouilletflorent@outlook.com>
2026-05-04 10:56:27 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+def FizzBuzz():
+    for i in range(1,21):
+        print(i)
+        if i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else :
+            print("Nope") 
+
+
+FizzBuzz()
+
